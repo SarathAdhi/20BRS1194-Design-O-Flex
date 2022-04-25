483919a7badd7f0dbc74229b1e7ca9d94d18bbdf
Sarath Adhithya <91727830+SarathAdhi@users.noreply.github.com>
2022-04-25 17:55:36 +0530

    Update Navbar.js

--- src/common/header/Navbar.js
+++ src/common/header/Navbar.js
@@ -17,14 +17,14 @@ const Navbar = () => {
                     </div>
 
                     <div className="navlinks">
-                        <a href="/">Home<img className='home-dimg' src="https://img.icons8.com/office/16/000000/home--v2.gif" /></a>
-                        <a href="/vr">Experience<img className='vr-dimg' src="https://w7.pngwing.com/pngs/158/474/png-transparent-dell-virtual-reality-headset-head-mounted-display-htc-vive-mixed-reality-vr-headset-electronics-visor-game-controllers.png" /></a>
-                        <a href="/product">Products<img className='product-dimg' src="https://img.icons8.com/color/48/000000/add-shopping-cart--v2.gif" /></a>
+                        <a href="/">Home<img className='home-dimg' src="https://img.icons8.com/office/16/000000/home--v2.gif" alt='' /></a>
+                        <a href="/vr">Experience<img className='vr-dimg' src="https://w7.pngwing.com/pngs/158/474/png-transparent-dell-virtual-reality-headset-head-mounted-display-htc-vive-mixed-reality-vr-headset-electronics-visor-game-controllers.png" alt='' /></a>
+                        <a href="/product">Products<img className='product-dimg' src="https://img.icons8.com/color/48/000000/add-shopping-cart--v2.gif" alt=''/></a>
 
-                        <a href="/contact">Contact Us<img className='contact-dimg' src="https://img.icons8.com/color/48/000000/name--v2.gif" /></a>
+                        <a href="/contact">Contact Us<img className='contact-dimg' src="https://img.icons8.com/color/48/000000/name--v2.gif" alt='' /></a>
 
                     </div>
-                    <img className="close-btn" src="https://img.icons8.com/ios-filled/50/000000/menu--v4.png" onClick={() => setisOpen(!isOpen)} />
+                    <img className="close-btn" src="https://img.icons8.com/ios-filled/50/000000/menu--v4.png" onClick={() => setisOpen(!isOpen)} alt='' />
 
                 </div>
             </div>
